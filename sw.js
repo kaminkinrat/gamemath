488cb8b24a4703e41b7bf3c38782890664396ed5
kaminkinrat <165889956+kaminkinrat@users.noreply.github.com>
2026-02-08 18:50:50 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = "mathgame-v1";
+const urlsToCache = [
+  "/gamemath/",
+  "/gamemath/index.html"
+];
+
+// ติดตั้ง
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// ดัก request
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// ลบ cache เก่าเมื่ออัปเดต
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
